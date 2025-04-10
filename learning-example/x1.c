97367eb960d421d717d9f72c12047a9bf91ee3cc
Asuka <2758843084@qq.com>
2025-04-10 20:37:09 +0800

    today

--- learning-example/x1.c
+++ learning-example/x1.c
@@ -2,37 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 #include <string.h>
+char str[1000][105];
+int len[1000];
+int id[1000];
 int cmp(const void* p1, const void* p2) {
 	int x = *(int*)p1;
 	int y = *(int*)p2;
-	if (x < y)
+	if (len[x]>len[y])
 		return -1;
-	if (x > y)
+	if (len[x] < len[y])
 		return 1;
 	else
 		return 0;
 
 }
-int a[10000];
+
 int main()
 {
 	int n;
 	scanf("%d", &n);
+	getchar();
 	for (int i = 0;i < n;i++)
 	{
-		scanf("%d", &a[i]);
-	}
-	qsort(a, n, sizeof(int), cmp);
-	int old = a[0];
-	printf("%d ", old);
-	for (int i = 1;i < n;i++)
-	{
-		if (old == a[i]) continue;
-		printf("%d ", a[i]);
-		old = a[i];
+		gets_s(str[i], 105);
+		len[i] = strlen(str[i]);
+		id[i] = i;
 
 	}
+	qsort(id, n, sizeof(int), cmp);
+	for (int i = 0;i < n;i++)
+	{
+		puts(str[id[i]]);
+	}
+	return 0;
 
 
 }
