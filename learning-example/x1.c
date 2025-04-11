124a13a79c70256aeaefb4677bdc64eec157f45d
Asuka <2758843084@qq.com>
2025-04-11 18:00:10 +0800

    lazy day

--- learning-example/x1.c
+++ learning-example/x1.c
@@ -1,20 +1,42 @@
+// 题目描述
+//给出若干个 int 范围内的整数，请你按照以下规则进行排序：
+//
+//以整数 32 位补码表示中 1 的个数为第一关键字升序排列；
+//以整数的值为第二关键字降序排列。
+//即，整数 32 位补码中 1 的个数较小者排在前面，若个数相等，则整数的值较大者排在前面。
 
+
+
+/*
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
+#include <malloc.h>
+#include <memory.h>
 #include <string.h>
-char str[1000][105];
-int len[1000];
-int id[1000];
+int a[100005];
+int f(int x)
+{
+	int b=0;
+	for (int i = 0;i < 32;i++)
+	{
+		if ((x >> i) & 1)
+			b++;
+	
+	}
+	return b;
+}
 int cmp(const void* p1, const void* p2) {
 	int x = *(int*)p1;
 	int y = *(int*)p2;
-	if (len[x]>len[y])
-		return -1;
-	if (len[x] < len[y])
+	if (f(x)>f(y))
 		return 1;
+	if (f(x) < f(y))
+		return -1;
+	if (x > y)
+		return -1;
+	if (x < y)
+		return 1;
+	
 	else
 		return 0;
 
@@ -27,17 +49,16 @@ int main()
 	getchar();
 	for (int i = 0;i < n;i++)
 	{
-		gets_s(str[i], 105);
-		len[i] = strlen(str[i]);
-		id[i] = i;
+		scanf("%d", &a[i]);
 
 	}
-	qsort(id, n, sizeof(int), cmp);
+	qsort(a, n, sizeof(int), cmp);
 	for (int i = 0;i < n;i++)
 	{
-		puts(str[id[i]]);
+		printf("%d %d\n", a[i],f(a[i]));
 	}
 	return 0;
 
 
 }
+*/
--- learning-example/x2.c
+++ learning-example/x2.c
@@ -1,20 +1,29 @@
-//#include<stdio.h>
-//void ExGCD(int a, int b, int* d, int* x, int* y)
-//{
-//	if (b == 0)
-//	{
-//		*d = a, * x = 1, * y = 0;
-//		return;
-//	}
-//	int dd, xx, yy;
-//	ExGCD(b, a % b, &dd, &xx, &yy);
-//	*d = dd, * x = yy, * y = xx - a / b * yy;
-//}
-//int main()
-//{
-//	int a, b, d, x, y;
-//	scanf("%d%d", &a, &b);
-//	ExGCD(a, b, &d, &x, &y);
-//	printf("%d = %d*(%d) + %d*(%d)", d, a, x, b, y);
-//	return 0;
-//}
+#include<stdio.h>
+void ExGCD(int a, int b, int* d, int* x, int* y)
+{
+    if (b == 0)
+    {
+		*d = a; //将a赋给d指向的变量
+		*x = 1; //将1赋给x指向的变量
+        *y = 0;
+
+        //将(a,1,0)分别赋给d,x,y指向的变量
+        return;
+    }
+    int dd, xx, yy; //分别代表伪代码中的d',x',y'
+    //根据伪代码，调用ExGCD，传入dd,xx,yy的【地址】获得计算结果
+    //根据伪代码进行计算，并赋给d,x,y指向的变量
+}
+int main()
+{
+    int a, b, d, x, y;
+	scanf("%d %d", &a, &b); //读入
+	ExGCD(a, b, &d, &x, &y); //调用ExGCD函数，传入d,x,y的【地址】获得计算结果
+	printf("%d =%d*(%d) + %d*(%d)\n", d, a,x,b, y); //输出
+    
+    
+    //读入
+    //调用ExGCD函数，传入d,x,y的【地址】获得计算结果
+    //输出
+    return 0;
+}
