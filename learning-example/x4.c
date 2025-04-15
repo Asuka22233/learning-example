bc02af9f24287602d1c0ded0579503774e0f3cd3
Asuka <2758843084@qq.com>
2025-04-15 20:25:22 +0800

    add a little

--- learning-example/x4.c
+++ learning-example/x4.c
@@ -1,50 +1,61 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//typedef struct _Interval
-//{
-//    int left, right; //存储左右端点大小
-//    int loc, roc; //表示左/右端点开闭性，0-开，1-闭
-//} Interval, * pInterval;
-//int cmp(const void* e1, const void* e2) //按区间左端点大小升序排序，当左端点相同时，闭区间在前，开
-//
-//{
-//    pInterval p = (pInterval)e1, q = (pInterval)e2;
-//    if (p->left > q->left) return 1;
-//    if (p->left < q->left) return -1;
-//    if (p->loc < q->loc) return 1;
-//    if (p->loc > q->loc) return -1;
-//    return 0;
-//}
-//Interval a[500005];
-//int main()
-//{
-//    int n = 0; //记录输入的区间数量
-//    char l, r;
-//    while (~scanf(" %c%d,%d%c", &l, &a[n].left, &a[n].right, &r))
-//    {
-//        a[n].loc = l == '[' ? 1 : 0;
-//        a[n].roc = r == ']' ? 1 : 0;
-//        ++n;
-//    }
-//    qsort(a, n, sizeof(Interval), cmp); //按区间左端点大小升序排序，当左端点相同时，闭区间在前，开
-//
-//    for (int i = 0; i < n;)
-//    {
-//        Interval t = a[i]; //记录本次合并结果，初始化t为本次合并的最初区间
-//        //只有区间a[i]左端点小于t右端点，或区间a[i]左端点等于t右端点且不全为开区间时，才能合并
-//        for (++i; i < n && (a[i].left < t.right || (a[i].left == t.right && (a[i].loc |
-//            t.roc))); ++i) //至少一个闭时a[i].loc|t.roc为1，若均为开则为0
-//        {
-//            if (a[i].right > t.right) //a[i]右端点大于t，更新t右端点及其开闭性为a[i]右端点及其开闭
-//
-//                t.right = a[i].right, t.roc = a[i].roc;
-//            else if (a[i].right == t.right) //a[i]右端点等于t，更新t右端点的开闭性（a[i]和t中有一
-//
-//                t.roc |= a[i].roc;
-//            //若a[i]右端点小于t，则不需要改变t
-//        }
-//        printf("%c%d,%d%c\n", t.loc ? '[' : '(', t.left, t.right, t.roc ? ']' : ')'); //输
-//
-//    }
-//    return 0;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 100
+#define INITIAL_CAPACITY 10
+
+// 定义一个结构体来存储整数对
+typedef struct {
+    int first;
+    int second;
+} Pair;
+
+
+
+int range[1000005]; //全局数组初始化均为0
+int main() {
+    int l, r; //分别表示区间左、右端点
+    int capacity = INITIAL_CAPACITY;
+    int count = 0;
+    char line[MAX_LINE_LENGTH];
+
+    while (fgets(line, sizeof(line), stdin) != NULL) {
+        // 去除换行符
+        line[strcspn(line, "\n")] = 0;
+        // 如果是空行则结束输入
+        if (line[0] == '\0') {
+            break;
+        }
+
+        char* token = strtok(line, " ");
+        if (token == NULL) {
+            fprintf(stderr, "输入格式有误，请输入有效的整数对。\n");
+            continue;
+        }
+        long num1 = strtol(token, NULL, 10);
+
+        token = strtok(NULL, " ");
+        if (token == NULL) {
+            fprintf(stderr, "输入格式有误，请输入有效的整数对。\n");
+            continue;
+        }
+        long num2 = strtol(token, NULL, 10);
+        // 存储整数对
+        l= (int)num1;
+        r = (int)num2;
+        count++;
+        if (r > range[l]) range[l] = r;
+    }
+    
+        
+    for (l = 0; l <= 1000000; ++l) {
+        if (!range[l]) continue; //数组该位置元素值为0，说明没有以该位置为左端点的区间，跳过
+        printf("%d ", l); //输出区间左端点
+        for (r = range[l]; l <= r; ++l) //继续遍历数组，合并区间，注意用相同的计数变量，注意循环条件
+            
+            if (range[l] > r) r = range[l]; //更新正在合并的区间右端点
+        printf("%d\n", --l); //最后依次++l后不满足l<=r，此时l=r+1, 因此应--l，再输出右端点
+    }
+    return 0;
+}
