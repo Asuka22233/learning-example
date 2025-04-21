f1a8966778a676193e309f7e41f898711657c394
Asuka <2758843084@qq.com>
2025-04-21 20:01:15 +0800

    Update x3.c

--- learning-example/x3.c
+++ learning-example/x3.c
@@ -5,9 +5,40 @@ typedef struct {
 	int x;
 	p y;
 } P,*p;
+ void f(int d,p a)
+{
+	 p b = a;
+	 for (int i = 0; i < d; i++)
+	 {
+		 p c = (p)malloc(sizeof(P));
+		 scanf("%d", &c->x);
+		 b->y = c;
+		 b = c;
+	 }
+	 b->y = NULL;
+	 return;
+
+
+}
 int main()
 {
 	int n, m;
+	scanf("%d %d", &n, &m);
+	p a = (p)malloc(sizeof(P) );
+	f(n, a);
+	for (int i = 0; i < m; i++)
+	{
+		int op, k, l;
+		scanf("%d %d %d", &op, &k, &l);
+		if (op == 1)
+		{
+
+		}
+		if (op == 0)
+		{
+
+		}
+	}
 
 	return 0;
 }
