c9f544699e6f87abf3b46ddf2c2bc7586d03ae6f
Asuka <2758843084@qq.com>
2025-04-21 20:27:20 +0800

    update

--- learning-example/x3.c
+++ learning-example/x3.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-typedef struct {
+typedef struct hj{
 	int x;
-	p y;
+	struct hj *y;
 } P,*p;
  void f(int d,p a)
 {
-	 p b = a;
+	 a->y = (p)malloc(sizeof(P));
+	 p b = a->y;
 	 for (int i = 0; i < d; i++)
 	 {
 		 p c = (p)malloc(sizeof(P));
@@ -20,6 +21,57 @@ typedef struct {
 
 
 }
+ void g(p a, int k, int l)
+ {
+	 p b = a;
+	 for (int i = 0; i < k; i++)
+	 {
+		 b= b->y;
+	 }
+	 if (b != NULL)
+	 {
+		 p c = (p)malloc(sizeof(P));
+		 c->x = l;
+		 c->y = b->y;
+		 printf("Success!\n");
+	 }
+	 else
+	 {
+		 printf("Fail!\n");
+	 }
+ }
+ void h(p a, int k, int l)
+ {
+	 p b = a->y;
+	 while (b != NULL)
+	 {
+		 if (b->x == k)
+		 {
+			 p c = (p)malloc(sizeof(P));
+			 c->x = l;
+			 c->y = b->y;
+			 printf("Success!\n");
+			 return;
+		 }
+		 b = b->y;
+	 }
+	 if (b == NULL)
+	 {
+		 printf("Fail!\n");
+	 }
+
+ }
+ void pt(p a)
+ {
+	 p b = a->y;
+	 while (b != NULL)
+	 {
+		 printf("%d ", b->x);
+		 b = b->y;
+	 }
+	 printf("\n");
+ }
+
 int main()
 {
 	int n, m;
@@ -32,13 +84,15 @@ int main()
 		scanf("%d %d %d", &op, &k, &l);
 		if (op == 1)
 		{
-
+			g(a, k, l);
 		}
 		if (op == 0)
 		{
+			h(a, k, l);
 
 		}
 	}
+	pt(a);
 
 	return 0;
 }
