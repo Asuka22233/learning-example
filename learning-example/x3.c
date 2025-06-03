ce5bc5f0f56cc7f0ff00596c7c2541476fb38fb9
Asuka <2758843084@qq.com>
2025-06-03 20:38:32 +0800

    update

--- learning-example/x3.c
+++ learning-example/x3.c
@@ -1,73 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define MAX_SIZE 1000
-
-int compare(const void* a, const void* b) {
-    long long x = *(const long long*)a;
-    long long y = *(const long long*)b;
-    return (x < y) ? -1 : (x > y) ? 1 : 0;
-}
-
-int main() {
-    long long arr1[MAX_SIZE], arr2[MAX_SIZE];
-    long long common[MAX_SIZE], unique1[MAX_SIZE], unique2[MAX_SIZE];
-    int size1 = 0, size2 = 0, common_size = 0, unique1_size = 0, unique2_size = 0;
-
-    // 读取第一个数组
-    printf("请输入第一个数组（以非数字结束）：\n");
-    while (size1 < MAX_SIZE && scanf("%lld", &arr1[size1]) == 1) {
-        size1++;
-    }
-    // 清除输入缓冲区
-    while (getchar() != '\n');
-
-    // 读取第二个数组
-    printf("请输入第二个数组（以非数字结束）：\n");
-    while (size2 < MAX_SIZE && scanf("%lld", &arr2[size2]) == 1) {
-        size2++;
-    }
-
-    // 排序两个数组
-    qsort(arr1, size1, sizeof(long long), compare);
-    qsort(arr2, size2, sizeof(long long), compare);
-
-    // 查找公共元素并生成去重后的唯一元素数组
-    int i = 0, j = 0;
-    while (i < size1 && j < size2) {
-        if (arr1[i] < arr2[j]) {
-            unique1[unique1_size++] = arr1[i++];
-        }
-        else if (arr1[i] > arr2[j]) {
-            unique2[unique2_size++] = arr2[j++];
-        }
-        else {
-            common[common_size++] = arr1[i++];
-            j++; // 跳过重复比较
-        }
-    }
-    // 添加剩余元素
-    while (i < size1) unique1[unique1_size++] = arr1[i++];
-    while (j < size2) unique2[unique2_size++] = arr2[j++];
-
-    // 输出结果
-    printf("相同的元素：");
-    for (i = 0; i < common_size; i++) {
-        printf("%lld ", common[i]);
-    }
-    printf("\n");
-
-    printf("第一个数组去掉相同元素后：");
-    for (i = 0; i < unique1_size; i++) {
-        printf("%lld ", unique1[i]);
-    }
-    printf("\n");
-
-    printf("第二个数组去掉相同元素后：");
-    for (i = 0; i < unique2_size; i++) {
-        printf("%lld ", unique2[i]);
-    }
-    printf("\n");
-
-    return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//#define MAX_SIZE 1000
+//
+//int compare(const void* a, const void* b) {
+//    long long x = *(const long long*)a;
+//    long long y = *(const long long*)b;
+//    return (x < y) ? -1 : (x > y) ? 1 : 0;
+//}
+//
+//int main() {
+//    long long arr1[MAX_SIZE], arr2[MAX_SIZE];
+//    long long common[MAX_SIZE], unique1[MAX_SIZE], unique2[MAX_SIZE];
+//    int size1 = 0, size2 = 0, common_size = 0, unique1_size = 0, unique2_size = 0;
+//
+//    // 读取第一个数组
+//    printf("请输入第一个数组（以非数字结束）：\n");
+//    while (size1 < MAX_SIZE && scanf("%lld", &arr1[size1]) == 1) {
+//        size1++;
+//    }
+//    // 清除输入缓冲区
+//    while (getchar() != '\n');
+//
+//    // 读取第二个数组
+//    printf("请输入第二个数组（以非数字结束）：\n");
+//    while (size2 < MAX_SIZE && scanf("%lld", &arr2[size2]) == 1) {
+//        size2++;
+//    }
+//
+//    // 排序两个数组
+//    qsort(arr1, size1, sizeof(long long), compare);
+//    qsort(arr2, size2, sizeof(long long), compare);
+//
+//    // 查找公共元素并生成去重后的唯一元素数组
+//    int i = 0, j = 0;
+//    while (i < size1 && j < size2) {
+//        if (arr1[i] < arr2[j]) {
+//            unique1[unique1_size++] = arr1[i++];
+//        }
+//        else if (arr1[i] > arr2[j]) {
+//            unique2[unique2_size++] = arr2[j++];
+//        }
+//        else {
+//            common[common_size++] = arr1[i++];
+//            j++; // 跳过重复比较
+//        }
+//    }
+//    // 添加剩余元素
+//    while (i < size1) unique1[unique1_size++] = arr1[i++];
+//    while (j < size2) unique2[unique2_size++] = arr2[j++];
+//
+//    // 输出结果
+//    printf("相同的元素：");
+//    for (i = 0; i < common_size; i++) {
+//        printf("%lld ", common[i]);
+//    }
+//    printf("\n");
+//
+//    printf("第一个数组去掉相同元素后：");
+//    for (i = 0; i < unique1_size; i++) {
+//        printf("%lld ", unique1[i]);
+//    }
+//    printf("\n");
+//
+//    printf("第二个数组去掉相同元素后：");
+//    for (i = 0; i < unique2_size; i++) {
+//        printf("%lld ", unique2[i]);
+//    }
+//    printf("\n");
+//
+//    return 0;
+//}
--- learning-example/x5.c
+++ learning-example/x5.c
@@ -1,61 +1,115 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//#define MAX_SIZE 500005
-//
-//int a[MAX_SIZE];
-//int b[MAX_SIZE];
-//
-//// 简化的比较函数
-//int cmp(const void* p1, const void* p2) {
-//    return (*(int*)p1 - *(int*)p2);
-//}
-//
-//// 二分查找函数
-//int binarySearch(int arr[], int n, int target) {
-//    int left = 0, right = n - 1;
-//    while (left <= right) {
-//        int mid = left + (right - left) / 2;
-//        if (arr[mid] < target) {
-//            left = mid + 1;
-//        }
-//        else {
-//            right = mid - 1;
-//        }
-//    }
-//    return left;
-//}
-//
-//int main() {
-//    int n;
-//    scanf("%d", &n);
-//
-//    // 读取数组 a
-//    for (int i = 0; i < n; i++) {
-//        scanf("%d", &a[i]);
-//    }
-//
-//    // 读取数组 b
-//    for (int i = 0; i < n; i++) {
-//        scanf("%d", &b[i]);
-//    }
-//
-//    // 对数组 a 和 b 进行排序
-//    qsort(a, n, sizeof(int), cmp);
-//    qsort(b, n, sizeof(int), cmp);
-//
-//    int flag = -1;
-//    for (int i = 0; i < n; i++) {
-//        // 使用二分查找找到第一个大于等于 a[i] 的元素的索引
-//        int index = binarySearch(b, n, a[i]);
-//        if (index == n) {
-//            flag = i;
-//            break;
-//        }
-//    }
-//
-//    // 输出结果
-//    printf("%d\n", flag);
-//
-//    return 0;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 105
+
+
+int f(char op) {
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
+    return 0;
+}
+
+
+void g(char* a, char* b) {
+    char stack[MAX];
+    int top = -1;
+    int j = 0;
+
+    for (int i = 0; a[i] != '\0'; i++) {
+        if (isdigit(a[i])) {
+
+            while (isdigit(a[i])) {
+                b[j++] = a[i++];
+            }
+            b[j++] = ' ';
+            i--;
+        }
+        else if (a[i] == '(') {
+
+            stack[++top] = a[i];
+        }
+        else if (a[i] == ')') {
+
+            while (top >= 0 && stack[top] != '(') {
+                b[j++] = stack[top--];
+                b[j++] = ' ';
+            }
+            top--;
+        }
+        else if (a[i] == '+' || a[i] == '-' || a[i] == '*' || a[i] == '/') {
+
+            while (top >= 0 && f(stack[top]) >= f(a[i])) {
+                b[j++] = stack[top--];
+                b[j++] = ' ';
+            }
+            stack[++top] = a[i];
+        }
+    }
+
+    while (top >= 0) {
+        b[j++] = stack[top--];
+        b[j++] = ' ';
+    }
+    b[j] = '\0';
+}
+
+double z(char* b) {
+    double stack[MAX];
+    int top = -1;
+
+    for (int i = 0; b[i] != '\0'; i++) {
+        if (isdigit(b[i])) {
+
+            double num = 0;
+            while (isdigit(b[i])) {
+                num = num * 10 + (b[i] - '0');
+                i++;
+            }
+            stack[++top] = num;
+        }
+        else if (b[i] == '+' || b[i] == '-' || b[i] == '*' || b[i] == '/') {
+
+            double operand2 = stack[top--];
+            double operand1 = stack[top--];
+            switch (b[i]) {
+            case '+':
+                stack[++top] = operand1 + operand2;
+                break;
+            case '-':
+                stack[++top] = operand1 - operand2;
+                break;
+            case '*':
+                stack[++top] = operand1 * operand2;
+                break;
+            case '/':
+                stack[++top] = operand1 / operand2;
+                break;
+            }
+        }
+    }
+
+    return stack[top];
+}
+
+int main() {
+    char a[MAX];
+    char b[MAX];
+
+
+    fgets(a, MAX, stdin);
+
+    a[strcspn(a, "=\n")] = '\0';
+
+    g(a, b);
+
+    double result = z(b);
+
+    printf("%.2f\n", result);
+
+    return 0;
+}
