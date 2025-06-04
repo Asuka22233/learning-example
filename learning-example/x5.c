333c7d0cadc6682863cc06e91282a0af4a6efcf3
Asuka <2758843084@qq.com>
2025-06-04 20:02:39 +0800

    Update x5.c

--- learning-example/x5.c
+++ learning-example/x5.c
@@ -1,31 +1,64 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-typedef struct DNode {
+typedef int ElemType;
+int maxsize;
+ElemType stack[10000];
+int top = -1;
+int isEmpty()
+{
+	return top == -1;
+}
+int isFull()
+{
+	return top == maxsize - 1;
+}
+void Error(char s[]) {
+	puts(s);
+	exit(-1);
 
-	int data;
-	struct DNode* prior, * next;
+}
+void push(ElemType item) {
+	if (isFull())
+		Error("Full Stack!");
+	else
+		stack[++top] = item;
 
-}DNode,*DLinklist;
 
-void ListInsert(DLinklist L, int t, int e) {
+}
+ElemType pop() {
+	if (isEmpty())
+		Error("Empty Stack!");
+	else
+		return stack[top--];
 
-	for (DLinklist p = L->next, q;p;p = p->next) {
-		if (p->data == t) {
-			q = (DNode*)malloc(sizeof(DNode));
-			q->data = e;
-			q->next = p->next;
-			if (p->next)
-				p->next->prior = q;
-			p->next = q;
-			q->prior = p;
-			return;
+}
+int main()
+{
+	int m;
 
-		
+	scanf("%d%d", &maxsize, &m);
+	while (m--) {
+
+		char op[10];
+		scanf("%s", op);
+		if (strcmp(op, "show") == 0) {
+			printf("%d\nStack:", top + 1);
+			for (int i = 0;i <= top;++i)
+				printf("%d ", stack[i]);
+			putchar('\n');
+
 		}
-		
+		else if (strcmp(op, "push") == 0) {
+			int item=0;
+			scanf("%d", item);
+			push(item);
 
-
+		}
+		else {
+			printf("%d", pop());
+		}
 	}
-	puts("Not Found!");
+	return 0;
+
 }
