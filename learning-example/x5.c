fb18a4346d3a338b5e5d6b2c4d2353d268300785
Asuka <2758843084@qq.com>
2025-06-04 15:36:49 +0800

    Update x5.c

--- learning-example/x5.c
+++ learning-example/x5.c
@@ -1,27 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
-int cmp(const void* p1, const void* p2) {
-	int* a = (int*)p1, * b = (int*)p2;
-	if (*a < *b)return -1;
-	else if (*a > *b)return 1;
-	else return 0;
+#include<string.h>
+char str[100000][105];
+int len[100000];
+int id[100000];
+
+int cmp(const void* p1,const void* p2)
+{
+	int a = *(int*)p1, b = *(int*)p2;
+	if (len[a] < len[b]) return 1;
+	else if (len[a] > len[b])
+		return -1;
+	else return strcmp(str[a], str[b]);
 
 }
-int a[1000005];
+
+
+
+
+
+
+
+
 int main() {
 	int n;
 	scanf("%d", &n);
-	for (int i = 0;i < n;++i) {
-		scanf("%d", &a[i]);
+	getchar();
+	for (int i = 0;i < n;i++) {
+		gets(str[i]);
+		len[i] = strlen(str[i]);
+		id[i] = i;
+
 	}
-	qsort(a, n, sizeof(int), cmp);
-	int old = a[0];
-	printf("%d ", old);
-	for (int i = 1;i < n;++i) {
-		if (old == a[i]) continue;
-		printf("%d ", a[i]);
-		old = a[i];
+	qsort(id, n, sizeof(int), cmp);
+	for (int i = 0;i < n;++i) {
+		puts(str[id[i]]);
+
 	}
 	return 0;
-
 }
