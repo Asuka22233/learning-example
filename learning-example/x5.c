bf173d823cb6482b076f8c5a4a816274d90d6f4d
Asuka <2758843084@qq.com>
2025-06-04 16:37:55 +0800

    Update x5.c

--- learning-example/x5.c
+++ learning-example/x5.c
@@ -1,26 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+char* s[10005];
+char str[1005];
+int main()
+{
+	int n, m;
+	scanf("%d%d", &n, &m);
+	getchar();
+	for (int i = 1; i <= n;i++)
+	{
+		gets(str);
+		int len = strlen(str);
+		s[i] = (char*)malloc((len + 1) * sizeof(char));
+		strcpy(s[i], str);
+	}
+	for (int i = 1;i <= m;i++) {
+		int u, v;
+		scanf("%d%d", &u, &v);
+		char* x = s[u];
+		s[u] = s[v];
+		s[v] = x;
 
-void ExGCD(int a, int b, int* d, int* x, int* y) {
-	if (b == 0) {
-		*d = a, * x = 1, * y = 0;
-		return;
 
 	}
-	int dd, xx, yy;
-	ExGCD(b, a % b, &dd, &xx, &yy);
-	*d = dd, * x = yy, * y = xx - a / b * yy;
+	for (int i = 1;i <= n;i++)
+	{
+		puts(s[i]);
+		free(s[i]);
+	}
 
-
-
-}
-
-
-
-int main() {
-	int a, b, d, x, y;
-	scanf("%d%d", &a, &b);
-	ExGCD(a, b, &d, &x, &y);
-	printf("%d=%d*(%d)+%d*(%d)", d, a, x, b, y);
 	return 0;
 }
+
