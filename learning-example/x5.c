d37458cdcb704dc37b2e229b053bc4e546bd1784
Asuka <2758843084@qq.com>
2025-06-04 18:35:55 +0800

    Update x5.c

--- learning-example/x5.c
+++ learning-example/x5.c
@@ -1,35 +1,97 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-char* s[10005];
-char str[1005];
-int main()
-{
-	int n, m;
-	scanf("%d%d", &n, &m);
-	getchar();
-	for (int i = 1; i <= n;i++)
-	{
-		gets(str);
-		int len = strlen(str);
-		s[i] = (char*)malloc((len + 1) * sizeof(char));
-		strcpy(s[i], str);
-	}
-	for (int i = 1;i <= m;i++) {
-		int u, v;
-		scanf("%d%d", &u, &v);
-		char* x = s[u];
-		s[u] = s[v];
-		s[v] = x;
+typedef struct _Node {
+	int data;
+	struct _Node* next;
 
+}Node,*List;
+
+List read_list(int n) {
+	List head = (List)malloc(sizeof(Node));
+	List tail = head;
+	for (int i = 0;i < n;++i) {
+		List node = (List)malloc(sizeof(node));
+		scanf("%d", &node->data);
+		tail->next = node;
+		tail = node;
 
 	}
-	for (int i = 1;i <= n;i++)
-	{
-		puts(s[i]);
-		free(s[i]);
-	}
+	tail->next = NULL;
+	return head;
 
-	return 0;
 }
 
+void insert_by_index(List list, int k, int data) {
+	List p = list;
+	for (int i = 0;i < k;++i) {
+		if (p == NULL) break;
+		p = p->next;
+
+
+	}
+	if (p == NULL) {
+		puts("Fail!");
+		return;
+	}
+
+	List node = (List)malloc(sizeof(Node));
+	node->data = data;
+	node->next = p->next;
+	p->next = node;
+	puts("Success!");
+
+
+}
+
+void insert_by_data(List list, int v, int data) {
+
+	List p = list->next;
+	while (p != NULL) {
+		if (p->data == v) {
+			List node = (List)malloc(sizeof(node));
+			node->data = data;
+			node->next = p->next;
+			p->next = node;
+			puts("Success!");
+			return;
+		}
+		else
+			p = p->next;
+
+	}
+	puts("Fail!");
+
+
+}
+void print_list(List list)
+{
+	for (List p = list->next;p;p = p->next)
+		printf("%d", p->data);
+	printf("\n");
+
+}
+
+int main() {
+
+	int n, m;
+	scanf("%d%d", &n, &m);
+	List list = read_list(n);
+	while (m--) {
+
+		int op, x, data;
+		if (op == 1)
+			insert_by_index(list, x, data);
+		else
+			insert_by_data(list, x, data);
+
+
+
+
+	}
+	print_list(list);
+	return 0;
+
+
+
+}
