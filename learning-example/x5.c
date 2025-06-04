760565e92a9043a743c9d584ef9d22a76eddef7b
Asuka <2758843084@qq.com>
2025-06-04 16:21:41 +0800

    Update x5.c

--- learning-example/x5.c
+++ learning-example/x5.c
@@ -1,38 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
-int popcount(int x) {
-	int sum = 0;
-	for (int i = 0;i < 32;i++) {
-		sum += x >> i & 1;
+
+void ExGCD(int a, int b, int* d, int* x, int* y) {
+	if (b == 0) {
+		*d = a, * x = 1, * y = 0;
+		return;
+
 	}
-	return sum;
-}
-int cmp(const void* p, const void* q) {
-	int x = *(int*)p,
-		y = *(int*)q;
-	int cntx = popcount(x),
-		cnty = popcount(y);
-	if (cntx > cnty) return 1;
-	if (cntx < cnty) return -1;
-	if (y > x) return 1;
-	if (x > y) return -1;
-	return 0;
+	int dd, xx, yy;
+	ExGCD(b, a % b, &dd, &xx, &yy);
+	*d = dd, * x = yy, * y = xx - a / b * yy;
+
 
 
 }
-int a[100000];
+
+
+
 int main() {
-
-	int n;
-	scanf("%d", &n);
-	for (int i = 0;i < n;i++) {
-		scanf("%d", &a[i]);
-
-	}
-	qsort(a, n, sizeof(int), cmp);
-	for (int i = 0;i < n;i++)
-		printf("%d %d", a[i], popcount(a[i]));
+	int a, b, d, x, y;
+	scanf("%d%d", &a, &b);
+	ExGCD(a, b, &d, &x, &y);
+	printf("%d=%d*(%d)+%d*(%d)", d, a, x, b, y);
 	return 0;
-
-
 }
