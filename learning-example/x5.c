c35e472341f8522d7e943440ac47a017a2b2b603
Asuka <2758843084@qq.com>
2025-06-04 21:17:16 +0800

    Update x5.c

--- learning-example/x5.c
+++ learning-example/x5.c
@@ -1,37 +1,120 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <ctype.h>
 
-int k;
-int f[10004];
-int st[10004];
-int top;
-int l[1004], r[1003];
-int main() {
-	scanf("%d", k);
-	for (int i = 1;i <= k;i++)
-		scanf("%d", &f[i]);
-	top = 0;
-	for (int i = 1;i <= k;i++) {
-
-		while (top && f[st[top]] <= f[i])
-			top--;
-		if (top)
-			l[i] = st[top];
-		st[++top] = i;
-	}
-	top = 0;
-	for (int i = k;i ;i--) {
-
-		while (top && f[st[top]] <= f[i])
-			top--;
-		if (top)
-			r[i] = st[top];
-		st[++top] = i;
-	}
-	for (int i = 1;i <= k;i++)
-		printf("%d %d\n", l[i], r[i]);
+int f(char op) {
+	if (op == '+' || op == '-')
+		return 1;
+	if (op == '*' || op == '/')
+		return 1;
 	return 0;
 
+}
+void g(char* a, char* b) {
+	char stack[104];
+	int top = -1;
+	int j = 0;
+	for (int i = 0;a[i] != '\0';i++) {
+		if (isdigit(a[i])) {
+			while (isdigit(a[i])) {
+				b[j++] = a[i++];
+			}
+			b[j++] = ' ';
+			i--;
+		}
+		else if (a[i] == '(') {
+			stack[top] = a[i];
+		}
+		else if (a[i] == ')') {
+			while (top >= 0 && stack[top] != '(') {
+				b[j++] = stack[top--];
+				b[j++] = ' ';
+
+			}
+			top--;
+		}
+		else if (a[i] == '+' || a[i] == '-' || a[i] == '*' || a[i] == '/') {
+			while (top >= 0 && f(stack[top]) >= f(a[i])) {
+				b[j++] = stack[top--];
+					b[j++] = ' ';
+
+			}
+			stack[++top] = a[i];
+
+		}
+		
+
+	}
+	while (top >= 0) {
+		b[j++] = stack[top--];
+		b[j++] = ' ';
+
+	}
+	b[j] = '\0';
+
+
+}
+
+double z(char* b) {
+	double stack[104];
+	int top = -1;
+	for (int i = 0;b[i] != '\0';i++) {
+		if (isdigit(b[i])) {
+			double num = 0;
+			while (isdigit(b[i])) {
+				num = num * 10 + (b[i] - '0');
+				i++;
+
+			}
+			stack[++top] = num;
+		}
+		else if (b[i] == '+' || b[i] == '-' || b[i] == '*' || b[i] == '/') {
+
+			double x2 = stack[top--];
+			double x1 = stack[top--];
+			switch(b[i]) {
+
+				case'+':
+					stack[++top] = x1 + x2;
+					break;
+				case'-':
+					stack[++top] = x1 - x2;
+					break;
+				case'*':
+					stack[++top] = x1 * x2;
+					break;
+				case'/':
+					stack[++top] = x1 / x2;
+					break;
+
+
+
+			}
+
+
+		}
+		
+
+	}
+
+	return stack[top];
+
+
+}
+
+
+
+
+
+int main() {
+	char a[105];
+	char b[105];
+	fgets(a, 105, stdin);
+	a[strcspn(a, "=\n")] = '\0';
+	g(a, b);
+	double result = z(b);
+	printf("%.2f\n", result);
+	return 0;
 
 }
